6dda01e7607c28fbb288514da311bdf418a713a5
이지영 <violentjy@gmail.com>
2019-09-01 19:19:44 +0900

    GithubReposActivity DataBinding 적용

--- app/src/main/kotlin/com/maryang/fastrxjava/ui/GithubReposActivity.kt
+++ app/src/main/kotlin/com/maryang/fastrxjava/ui/GithubReposActivity.kt
@@ -1,17 +1,17 @@
 package com.maryang.fastrxjava.ui
 
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.maryang.fastrxjava.R
-import com.maryang.fastrxjava.data.source.DefaultSingleObserver
-import com.maryang.fastrxjava.entity.GithubRepo
+import com.maryang.fastrxjava.databinding.ActivityGithubReposBinding
 import io.reactivex.disposables.CompositeDisposable
-import kotlinx.android.synthetic.main.activity_github_repos.*
 
 
 class GithubReposActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityGithubReposBinding
+
     private val disposable = CompositeDisposable()
 
     private val viewModel: GithubReposViewModel by lazy {
@@ -23,14 +23,16 @@ class GithubReposActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_github_repos)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_github_repos)
 
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = this.adapter
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = this.adapter
 
-        refreshLayout.setOnRefreshListener { load() }
+        binding.refreshLayout.setOnRefreshListener { load() }
 
-        load(true)
+        binding.viewModel = viewModel
+
+        load()
     }
 
     override fun onDestroy() {
@@ -39,32 +41,10 @@ class GithubReposActivity : AppCompatActivity() {
         disposable.dispose()
     }
 
-    private fun load(showLoading: Boolean = false) {
-        if (showLoading)
-            showLoading()
-
-        val repoDisposable = viewModel.getGithubRepos()
-            .subscribeWith(object: DefaultSingleObserver<List<GithubRepo>>() {
-                override fun onSuccess(result: List<GithubRepo>) {
-                    hideLoading()
-                    adapter.items = result
-                }
-
-                override fun onError(e: Throwable) {
-                    super.onError(e)
-                    hideLoading()
-                }
-            })
-
-        disposable.add(repoDisposable)
-    }
-
-    private fun showLoading() {
-        loading.visibility = View.VISIBLE
-    }
-
-    private fun hideLoading() {
-        loading.visibility = View.GONE
-        refreshLayout.isRefreshing = false
+    private fun load() {
+        disposable.clear()
+        disposable.add(
+            viewModel.getGithubRepos()
+        )
     }
 }
--- app/src/main/kotlin/com/maryang/fastrxjava/ui/GithubReposViewModel.kt
+++ app/src/main/kotlin/com/maryang/fastrxjava/ui/GithubReposViewModel.kt
@@ -1,21 +1,61 @@
 package com.maryang.fastrxjava.ui
 
+import android.view.View
+import androidx.databinding.BindingAdapter
+import androidx.databinding.ObservableArrayList
+import androidx.databinding.ObservableBoolean
+import androidx.recyclerview.widget.RecyclerView
 import com.maryang.fastrxjava.data.repository.GithubRepository
-import com.maryang.fastrxjava.data.source.DefaultCallback
 import com.maryang.fastrxjava.data.source.DefaultSingleObserver
 import com.maryang.fastrxjava.entity.GithubRepo
-import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
-import retrofit2.Call
-import retrofit2.Response
 
 class GithubReposViewModel {
     private val repository = GithubRepository()
 
-    fun getGithubRepos(): Single<List<GithubRepo>> {
+    val repos = ObservableArrayList<GithubRepo>()
+    val isLoading = ObservableBoolean()
+
+    fun getGithubRepos(): Disposable {
+        isLoading.set(true)
+
         return repository.getGithubRepos()
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
+            .subscribeWith(object: DefaultSingleObserver<List<GithubRepo>>() {
+                override fun onSuccess(t: List<GithubRepo>) {
+                    isLoading.set(false)
+
+                    repos.clear()
+                    repos.addAll(t)
+                }
+
+                override fun onError(e: Throwable) {
+                    super.onError(e)
+                    isLoading.set(false)
+                }
+            })
+    }
+
+    companion object {
+        @JvmStatic
+        @BindingAdapter("visibility")
+        fun setVisibility(view: View, isVisible: Boolean) {
+            view.visibility = if (isVisible) {
+                View.VISIBLE
+            } else {
+                View.GONE
+            }
+        }
+
+        @JvmStatic
+        @BindingAdapter("repos")
+        fun setRepos(recyclerView: RecyclerView, repos: ArrayList<GithubRepo>) {
+            val adapter = recyclerView.adapter as? GithubReposAdapter ?: return
+
+            adapter.items = repos
+        }
     }
 }
